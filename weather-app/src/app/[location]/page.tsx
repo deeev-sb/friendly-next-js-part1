9d74fbddfdcca5bf63299f7846d73e14bc4ac8eb
deeev-sb <sb2504@naver.com>
2024-02-11 19:02:55 +0900

    [Feat] ✨ Link, useRouter를 통한 페이지 이동 구현

--- weather-app/src/app/[location]/page.tsx
+++ weather-app/src/app/[location]/page.tsx
@@ -1,3 +1,7 @@
+'use client'
+
+import {useRouter} from "next/navigation";
+
 type Props = {
   params: {
     location: string
@@ -6,6 +10,12 @@ type Props = {
 
 export default function Detail({params}: Props) {
   let name = ''
+
+  const router = useRouter();
+  const handleClick = () => {
+    router.push('/')
+  }
+
   switch (params.location){
     case 'seoul': name = '서울'; break;
     case 'newyork': name = '뉴욕'; break;
@@ -14,6 +24,7 @@ export default function Detail({params}: Props) {
   return (
     <>
       <h1>{name}의 3일 예보</h1>
+      <button onClick={handleClick}>Home</button>
     </>
   );
 }
