11710a9cbc59b94080a45fc953c67bc771ade9bb
deeev-sb <sb2504@naver.com>
2024-02-11 20:47:42 +0900

    [Style] 💄 List, Button 스타일 변경

--- weather-app/src/app/page.tsx
+++ weather-app/src/app/page.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
+import style from './style.module.css'
 
 export default function Home() {
   return (
     <>
       <h1>Home</h1>
-      <ul>
+      <ul className={style.list}>
         <li><Link href="/seoul">서울</Link></li>
         <li><Link href="/newyork">뉴욕</Link></li>
         <li><Link href="/london">런던</Link></li>
