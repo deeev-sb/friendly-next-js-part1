400849b2933949132cfb8a772b7ae8065e9592a3
deeev-sb <sb2504@naver.com>
2024-02-12 15:41:15 +0900

    [Feat] ✨ 3일 예보 기능 구현

--- weather-app/src/app/[location]/page.tsx
+++ weather-app/src/app/[location]/page.tsx
@@ -1,4 +1,5 @@
 import HomeButton from "@/component/HomeButton";
+import {getForecast} from "@/utils/getForecast";
 
 type Props = {
   params: {
@@ -6,8 +7,10 @@ type Props = {
   }
 }
 
-export default function Detail({params}: Props) {
+export default async function Detail({params}: Props) {
   let name = ''
+
+  const res = await getForecast(params.location);
 
   switch (params.location){
     case 'seoul': name = '서울'; break;
@@ -18,6 +21,14 @@ export default function Detail({params}: Props) {
     <>
       <h1>{name}의 3일 예보</h1>
       <HomeButton/>
+      <ul>
+        {res.forecast.forecastday.map(day => (
+          <li key={day.date}>
+            {day.date} / {day.day.avgtemp_c}
+          </li>
+          )
+        )}
+      </ul>
     </>
   );
 }
